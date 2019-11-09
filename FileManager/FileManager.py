98228ac43f5f0fd1217377e93851461e7a5328e7
Diogo <diogo_pontes_95@hotmail.com>
2019-11-09 19:12:52 -0300

    Added File Manager logs

--- FileManager/FileManager.py
+++ FileManager/FileManager.py
@@ -3,35 +3,36 @@ class FileManager:
 		self.file_owner = []
 		for i in range(0, max_size):
 			self.file_owner.append(-1)
-	
+
 	def insert_file(self, disk, file_name, first_block, file_size):
 		for i in range(0, file_size):
 			disk.blocks[first_block + i] = file_name
 			self.file_owner[first_block + i] = -1
 		disk.free_space = disk.free_space - file_size
+		print("File Manager: File {} added to disk. Starting in block {} and size {}".format(file_name, first_block, file_size))
 
 	def create_file(self, disk, file_size, file_name, process_id):
 		if file_size > disk.free_space:
-			print("Disk out of space")
+			print("Disk out of space for file {}".format(file_name))
 		else:
 			block = self.find_block(disk, file_size)
 			if block == -1:
-				print("No contiguous blocks for file")
+				print("File Manager: No contiguous blocks for file {}".format(file_name))
 			else:
 				for i in range(0, file_size):
 					disk.blocks[block + i] = file_name
 					self.file_owner[block + i] = -1
 				disk.free_space = disk.free_space - file_size
-				
+				print("File Manager: File {} created. Starting in block {} and size {} by process {}".format(file_name, block, file_size, process_id))
 
 	def delete_file(self, disk, file_name, process_type, process_id):
 		try:
 			block = disk.blocks.index(file_name)
 		except ValueError:
-			print("File does not exist")
+			print("File Manager: File {} does not exist".format(file_name))
 		else:
 			if (self.check_owner(disk, file_name) != process_id) and (process_type != 0):
-				print("No permission to delete file")
+				print("File Manager: No permission to delete file {}".format(file_name))
 			else:
 				file_size = 0
 				for i in range(block, disk.max_size):
@@ -46,8 +47,8 @@ class FileManager:
 					else:
 						break;
 				disk.free_space = disk.free_space + file_size
-		
-			
+				print("File Manager: File {} deleted. Starting in block {} and size {} by process {}".format(file_name, block, file_size, process_id))
+
 	def check_owner(self, disk, file_name):
 		block = disk.blocks.index(file_name)
 
@@ -63,10 +64,10 @@ class FileManager:
 		return -1
 
 	def show_disk(self, disk):
-		print("File Manager:")
+		print("File Manager: Disk status:")
 		print("\tDisk Free Space:", disk.free_space)
 		print("\tDisk Map:", disk.blocks)
-		print("\tDisk Map Owners:", self.file_owner)
+		print("\tDisk Owners Map:", self.file_owner)
 
 class HardDisk:
 	def __init__(self, max_size):
